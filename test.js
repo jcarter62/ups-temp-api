4f44e577da95bf9e6d5b3e8e7bbd2f2aae9b8b8f
Jim Carter <jcarter62@gmail.com>
2018-08-20 17:16:26 -0700

    auth changes

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ let express = require('express');
 let sql = require('mssql');
 let router = express.Router();
 
-
 router.post('/', function( req, res ) {
   let dbConfig = req.app.locals.dbConfig;
   let result = {'api': '//' + req.headers.host + req.baseUrl };
@@ -16,8 +15,48 @@ router.post('/', function( req, res ) {
     } else {
         console.log('Connected');
         result.status = 'connected';
-        res.send(result);
-        sql.close();
+
+        let sp_result = '';
+        let sp_name = '';
+        let sp_email = '';
+        // Attempt to authorize
+        let request = new sql.Request();
+        request.stream = false;
+        request.input('username', sql.VarChar, '');
+        request.input('userpass', sql.VarChar, '');
+        request.output('AuthResult', sql.VarChar, sp_result);
+        request.output('name', sql.VarChar, sp_name);
+        request.output('email', sql.VarChar, sp_email);
+        request.execute('sp_mi_authorize', function(err, sqlResultl, returnVal, affected) {
+          if ( err ) {
+            console.log('login error');
+            result.results = sqlResultl;
+            res.send(result);
+            sql.close();
+          } else {
+            result.results = sqlResultl.output;
+            res.send(result);
+            sql.close();
+          }
+        });
+
+
+/*
+declare @username varchar(50) = 'jc';
+declare @userpass varchar(50) = 'wmis02';
+declare @result     varchar(30);
+declare @name           varchar(100);
+declare @email          varchar(100);
+declare @sep varchar(10) = ' / ';
+
+execute sp_mi_authorize @username, @userpass, @result out, @name out, @email out;
+
+select @result + @sep + @name + @sep + @email;
+
+
+ */
+
+
     }
   } );
 
